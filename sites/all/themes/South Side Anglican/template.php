dd950c64b835a8a4256d64a32070fec3fcd71df6
David Moore <davidmoore@david-moores-macbook-pro.local>
2012-04-21 12:52:56 -0400

    style changes 3

--- sites/all/themes/South Side Anglican/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-function phptemplate_menu_item($mid, $children = '', $leaf = TRUE) {
-  return _phptemplate_callback('menu_item', array(
-        'leaf' => $leaf,
-        'mid' => $mid,
-        'children' => $children
-    ));
-}
-  

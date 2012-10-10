fe79e96a642b6c1f18dc1ddd81020fabede28c01
David Moore <davidmoore@david-moores-macbook-pro.local>
2012-10-10 12:50:40 -0400

    various views updates

--- sites/all/themes/southsideanglican/node--art.tpl.php
+++ sites/all/themes/southsideanglican/node--art.tpl.php
@@ -81,6 +81,12 @@
 
 <?php if ($title): ?><h1><?php print render($title); ?></h1><?php endif; ?>
 
+<?php if ($title): ?><h1><?php print render($title); ?></h1><?php endif; ?>
+<div class="fb-like" data-send="false" data-layout="button_count" data-width="50" data-show-faces="false"></div>
+
+<a href="https://twitter.com/share" class="twitter-share-button" data-via="SSACpgh">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
 <?php
 $view = views_get_view('artwork_slider');
 $args = array($node->nid);

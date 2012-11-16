c4aac4b34f401a37ea483a26a137a0b866b0f4f1
David Moore <davidmoore@david-moores-macbook-pro.local>
2012-11-16 16:36:05 -0500

    fix for media queries issue attempt 2

--- sites/all/themes/hear-me-exhibit/html.tpl.php
+++ sites/all/themes/hear-me-exhibit/html.tpl.php
@@ -5,6 +5,41 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+
+    <style type="text/css" media="
+only screen and (min-width: 320px),
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 320px),
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 320px),
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 320px),
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 320px),
+only screen and (                min-resolution: 192dpi) and (min-width: 320px),
+only screen and (                min-resolution: 2dppx)  and (min-width: 320px) { 
+">
+
+  @import url('/ssac/sites/all/themes/hear-me-exhibit/css/stylesheets/overall.css');
+  @import url('/ssac/sites/all/themes/hear-me-exhibit/css/stylesheets/screen-small.css');
+
+
+</style>
+  
+    <style type="text/css" media="
+only screen and (min-width: 700px),
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 700px),
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 700px),
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 700px),
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 700px),
+only screen and (                min-resolution: 192dpi) and (min-width: 700px),
+only screen and (                min-resolution: 2dppx)  and (min-width: 700px)">
+
+  @import url('/ssac/sites/all/themes/hear-me-exhibit/css/stylesheets/overall.css');
+  @import url('/ssac/sites/all/themes/hear-me-exhibit/css/stylesheets/screen-small.css');
+  @import url('/ssac/sites/all/themes/hear-me-exhibit/css/stylesheets/screen.css');
+
+</style>
+
+
+
+
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 

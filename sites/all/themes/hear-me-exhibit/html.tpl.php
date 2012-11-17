100d61c682b3aed8a1a423d0cba003d6d38bde93
David Moore <davidmoore@david-moores-macbook-pro.local>
2012-11-16 20:47:58 -0500

    sldkjflsdjkfsl

--- sites/all/themes/hear-me-exhibit/html.tpl.php
+++ sites/all/themes/hear-me-exhibit/html.tpl.php
@@ -2,6 +2,9 @@
 <html>
 <head profile="<?php print $grddl_profile; ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>

409449550f66c84550330d45aa171359a35a6783
David Moore <davidmoore@david-moores-macbook-pro.local>
2012-11-19 19:44:17 -0500

    header space and chrome columns fix

--- sites/all/themes/hear-me-exhibit/scripts/custom.js
+++ sites/all/themes/hear-me-exhibit/scripts/custom.js
@@ -14,9 +14,8 @@
 
 			document.location = '/hear-me-exhibit';});
 
-		$("div.view-hear-me-your-story-through-tattoos- div.view-content .views-row").addClass('fade-in');
+		// $("div.view-hear-me-your-story-through-tattoos- div.view-content .views-row").addClass('fade-in');
 
-		$("body").iealert();
 	});
 
 })(jQuery);
